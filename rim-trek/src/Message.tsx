e8a04ce1b0278fb83dfa2afdb86f0f2675544c8b
Lamia Jabin Rimty <rimtycse@gmail.com>
2024-12-12 12:33:51 +0100

    importing bootstrap files and wraping shortcuts

---
+++ rim-trek/src/Message.tsx
@@ -0,0 +1,17 @@
+function Message(){
+    const name = " ";
+    if(name){
+        return(
+            <h1>HaPpY Trekking {name} </h1>
+         ) 
+    }
+    else{
+        return(
+            <h1>HaPpY Trekking </h1>
+         ) 
+    }
+    
+
+}
+
+export default Message;
---
+++ rim-trek/src/components/list-group.tsx
@@ -0,0 +1,17 @@
+//capitalising 1st letter each word Pascal convention
+
+function ListofGroup() {
+  return (
+    
+      <h1>Lists of Trekking</h1>
+      <ul className="list-group">
+        <li className="list-group-item">An item</li>
+        <li className="list-group-item">A second item</li>
+        <li className="list-group-item">A third item</li>
+        <li className="list-group-item">A fourth item</li>
+        <li className="list-group-item">And a fifth one</li>
+      </ul>
+    
+  );
+}
+export default ListofGroup;
