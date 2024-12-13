9db465f97e613ecbf408280dfed48affa26c2246
Lamia Jabin Rimty <rimtycse@gmail.com>
2024-12-13 15:54:37 +0100

    handling events

--- rim-trek/src/components/list-group.tsx
+++ rim-trek/src/components/list-group.tsx
@@ -1,3 +1,5 @@
+import { MouseEvent } from "react";
+
 //capitalising 1st letter each word Pascal convention
 
 function ListofGroup() {
@@ -10,18 +12,17 @@ function ListofGroup() {
     "Italy",
   ];
   //items = [];
+  function handleClick(event: MouseEvent) {
+    console.log(event);
+  }
 
   return (
     <>
       <h1>List of my Trek countries </h1>
       {items.length === 0 && <p>Countries not found!</p>}
       <ul className="list-group">
-        {items.map((item) => (
-          <li
-            className="list-group-item"
-            key={item}
-            onClick={() => console.log("Clicked")}
-          >
+        {items.map((item, index) => (
+          <li className="list-group-item" key={item} onClick={handleClick}>
             {item}
           </li>
         ))}
